201f296600d0190a83f67de73fe78841d6ae8ade
Davronn <davronmahamadjonov8@gmai.com>
2025-03-08 22:28:06 +0500

    upload project git hub

---
+++ data/products.js
@@ -0,0 +1,65 @@
+const products = [
+  {
+    id: 1,
+    name: "Uzuk",
+    price: 200,
+    image: "images/p1.png",
+    isNew: true
+  },
+  {
+    id: 2,
+    name: "Soat",
+    price: 300,
+    image: "images/p2.png",
+    isNew: true
+  },
+  {
+    id: 3,
+    name: "Yumshoq o'yinchoq",
+    price: 110,
+    image: "images/p3.png",
+    isNew: true
+  },
+  {
+    id: 4,
+    name: "Gul dasta",
+    price: 45,
+    image: "images/p4.png",
+    isNew: true
+  },
+  {
+    id: 5,
+    name: "Yumshoq o'yinchoq",
+    price: 95,
+    image: "images/p5.png",
+    isNew: true
+  },
+  {
+    id: 6,
+    name: "Gul dasta",
+    price: 70,
+    image: "images/p6.png",
+    isNew: true
+  },
+  {
+    id: 7,
+    name: "Soat",
+    price: 400,
+    image: "images/p7.png",
+    isNew: false
+  },
+  {
+    id: 8,
+    name: "Uzuk",
+    price: 450,
+    image: "images/p8.png",
+    isNew: true
+  },
+  {
+    id: 9,
+    name: "Uzuk",
+    price: 450,
+    image: "images/p8.png",
+    isNew: false
+  }
+]; 

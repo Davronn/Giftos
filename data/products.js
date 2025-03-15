2bf06100d6188dc9467e79bc5c2e0b3a0d602543
Davronn <davronmahamadjonov8@gmai.com>
2025-03-15 21:20:14 +0500

    Mahsulotlarni qo'shishgacha tayyor

--- data/products.js
+++ data/products.js
@@ -1,65 +1,112 @@
 const products = [
   {
-    id: 1,
-    name: "Uzuk",
-    price: 200,
-    image: "images/p1.png",
-    isNew: true
-  },
-  {
-    id: 2,
-    name: "Soat",
-    price: 300,
-    image: "images/p2.png",
-    isNew: true
-  },
-  {
-    id: 3,
-    name: "Yumshoq o'yinchoq",
-    price: 110,
-    image: "images/p3.png",
-    isNew: true
-  },
-  {
-    id: 4,
+    id: "1",
     name: "Gul dasta",
-    price: 45,
+    price: 75,
     image: "images/p4.png",
-    isNew: true
+    isNew: true,
+    description: "Go'zal va nafis gullar to'plami. Har qanday bayram uchun ajoyib sovg'a.",
+    features: [
+      "Tarkibida 15 ta atirgul",
+      "Qo'shimcha bezaklar",
+      "Maxsus o'rov qog'ozi",
+      "Bepul yetkazib berish"
+    ]
   },
   {
-    id: 5,
-    name: "Yumshoq o'yinchoq",
-    price: 95,
-    image: "images/p5.png",
-    isNew: true
-  },
-  {
-    id: 6,
-    name: "Gul dasta",
-    price: 70,
-    image: "images/p6.png",
-    isNew: true
-  },
-  {
-    id: 7,
-    name: "Soat",
-    price: 400,
+    id: "2",
+    name: "Smart soat",
+    price: 150000,
     image: "images/p7.png",
-    isNew: false
+    isNew: true,
+    description: "Zamonaviy smart soat. Barcha kerakli funksiyalar mavjud.",
+    features: [
+      "Yurak urishi monitori",
+      "Suv o'tkazmaydigan",
+      "5 kun batareya muddati",
+      "GPS funksiyasi"
+    ]
   },
   {
-    id: 8,
-    name: "Uzuk",
-    price: 450,
-    image: "images/p8.png",
-    isNew: true
+    id: "3",
+    name: "Yumshoq ayiqcha",
+    price: 90000,
+    image: "images/p5.png",
+    isNew: true,
+    description: "Yumshoq ayiqcha bolalar uchun",
+    features: [
+      "1 ta ayiqcha",
+      "bo'yinboq",
+    ]
   },
   {
-    id: 9,
-    name: "Uzuk",
-    price: 450,
+    id: "4",
+    name: "Charm hamyon",
+    price: 65000,
+    image: "images/purse.png",
+    isNew: false,
+    description: "Yuqori sifatli charm hamyon. Ko'p xonali va qulay dizayn.",
+    features: [
+      "100% tabiiy charm",
+      "Ko'p xonali",
+      "RFID himoyasi",
+      "2 yillik kafolat"
+    ]
+  },
+  {
+    id: "5",
+    name: "Zargarlik to'plami",
+    price: 120000,
+    image: "images/p1.png",
+    isNew: true,
+    description: "Ayollar uchun zargarlik to'plami. Zamonaviy va nafis dizayn.",
+    features: [
+      "Marvarid marjonlar",
+      "Oltin suvi yugurtirilgan",
+      "Quloqchinlar to'plami",
+      "Maxsus quti"
+    ]
+  },
+  {
+    id: "6",
+    name: "Kosmetika to'plami",
+    price: 85000,
+    image: "images/p6.png",
+    isNew: false,
+    description: "Professional kosmetika to'plami. Barcha kerakli vositalar bir to'plamda.",
+    features: [
+      "Organik tarkib",
+      "Gipoallergenik",
+      "10 ta produkt",
+      "Chiroyli sumka"
+    ]
+  },
+  {
+    id: "7",
+    name: "Smart soat",
+    price: 150000,
+    image: "images/p7.png",
+    isNew: true,
+    description: "Zamonaviy smart soat. Barcha kerakli funksiyalar mavjud.",
+    features: [
+      "Yurak urishi monitori",
+      "Suv o'tkazmaydigan",
+      "5 kun batareya muddati",
+      "GPS funksiyasi"
+    ]
+  },
+  {
+    id: "8",
+    name: "Qahva to'plami",
+    price: 55000,
     image: "images/p8.png",
-    isNew: false
-  }
+    isNew: false,
+    description: "Premium qahva donlari to'plami. Turli xil mamlakatlardan keltirilgan.",
+    features: [
+      "4 xil qahva turi",
+      "Maxsus yanchgich",
+      "Qahva filtrlari",
+      "Ko'rsatmalar kitobi"
+    ]
+  },
 ]; 
